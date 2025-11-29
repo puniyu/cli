f4d1fa9d66009734caad9c65a50b9d851ef4638c
wuliya <3369906077@qq.com>
2025-11-29 17:19:53 +0800

    chore: 修正

--- puniyu_cli/src/lib.rs
+++ puniyu_cli/src/lib.rs
@@ -1,36 +1,34 @@
 mod command;
 mod template;
 
-
-
 use clap::{Parser, Subcommand};
-use include_dir::{include_dir, Dir};
+use include_dir::{Dir, include_dir};
 
 pub(crate) static TEMPLATE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/template");
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct App {
-	#[command(subcommand)]
-	dev: Commands
+    #[command(subcommand)]
+    dev: Commands,
 }
 
 #[derive(Subcommand)]
-enum Commands  {
-	Create,
-	Dev
+enum Commands {
+    Create,
+    Dev,
 }
 
 impl App {
-	pub fn run<I, T>(args: I)
-	where
-		I: IntoIterator<Item = T>,
-		T: Into<std::ffi::OsString> + Clone,
-	{
-		let app = Self::parse_from(args);
-		match app.dev {
-			Commands::Create => command::create::Command::run(),
-			Commands::Dev => command::dev::Command::run(),
-		}
-	}
+    pub fn run<I, T>(args: I)
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<std::ffi::OsString> + Clone,
+    {
+        let app = Self::parse_from(args);
+        match app.dev {
+            Commands::Create => command::create::Command::run(),
+            Commands::Dev => command::dev::Command::run(),
+        }
+    }
 }
